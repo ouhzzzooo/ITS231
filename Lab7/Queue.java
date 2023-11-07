8a3d9f92bdf13593c297e8d904e7a67327b78a2d
Pongsakorn <107350719+ouhzzzooo@users.noreply.github.com>
2023-11-08 01:54:21 +0700

    not finish

---
+++ Lab7/Queue.java
@@ -0,0 +1,114 @@
+package Lab7;
+
+public class Queue<T> {
+	/** The underlying singly linked list for the queue. */
+	SList<T> list = new SList<T>();
+
+	/** Default constructor to initialize an empty queue. */
+	Queue() {
+	}
+
+	/**
+	 * Enqueues an element into the queue.
+	 *
+	 * @param element the element to enqueue
+	 */
+	void enqueue(T element) {
+		// Exercise 1a
+		list.addLast(element);
+	}
+
+	/**
+	 * Dequeues an element from the queue.
+	 *
+	 * @return the dequeued element
+	 */
+	T dequeue() {
+		return list.removeFirst(); // Exercise 1b
+	}
+
+	/**
+	 * Gets the element at the front of the queue without removing it.
+	 *
+	 * @return the element at the front of the queue
+	 */
+	T queuefront() {
+		return list.first.element; // Exercise 1c
+	}
+
+	/**
+	 * Gets the element at the rear of the queue without removing it.
+	 *
+	 * @return the element at the rear of the queue
+	 */
+	T queuerear() {
+		return list.last.element; // Exercise 1d
+	}
+
+	/**
+	 * Checks if the queue is empty.
+	 *
+	 * @return true if the queue is empty, false otherwise
+	 */
+	boolean isEmpty() {
+		return list.isEmpty();
+	}
+
+	/**
+	 * Creates a copy of this queue.
+	 *
+	 * @return a new queue containing the same elements as this queue
+	 */
+	Queue<T> copyQueue() { // Exercise 2
+		Queue<T> Q2 = new Queue<T>();
+		// Add your code here
+		for (int i = 0; i < list.size; i++) {
+			T tmp = dequeue();
+			Q2.enqueue(tmp);
+			enqueue(tmp);
+			
+		}
+		return Q2;
+		}
+		
+	
+
+	/**
+	 * Checks if this queue is identical to another queue in terms of content and
+	 * sequence.
+	 *
+	 * @param Q2 the queue to compare with
+	 * @return true if the queues are identical, false otherwise
+	 */
+	boolean isIdentical(Queue<T> Q2) {
+		if (list.size != Q2.list.size) {
+			return false;
+		}
+		for (int i = 0; i < list.size; i++) {
+			T tmp = dequeue();
+			T tt = Q2.dequeue();
+			if (tmp == tt) {
+				enqueue(tmp);
+				Q2.enqueue(tt);
+			}
+			else {
+				return false;
+			}
+		}
+		return true; // Exercise 3
+	}
+
+	/**
+	 * Prints the elements of the queue horizontally. Handles potential
+	 * 
+	 */
+	void printHorizontal() {
+		Node<T> walker = list.first;
+		while (walker != null) {
+			System.out.print(walker.element + " ");
+			walker = walker.next;
+		}
+
+	}
+
+}
